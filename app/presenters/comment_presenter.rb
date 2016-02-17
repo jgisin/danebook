1686db3a6eb28fc53c4dcbfcdd9b99dd2fa242dd
Jeffrey Gisin <jgisin@gmail.com>
2016-02-16 19:46:40 -0700

    like text

--- app/presenters/comment_presenter.rb
+++ app/presenters/comment_presenter.rb
@@ -25,4 +25,38 @@ class CommentPresenter < BasePresenter
     end
   end
 
+  def current_user_like?
+    liked = comment.likes.select{|like| like.user_id == h.current_user.id}
+    liked.length > 0
+  end
+
+  def users_name(id)
+    User.find(id).profile.fullname
+  end
+
+  def like_text
+    like_text = ""
+    if current_user_like?
+      like_text += h.link_to("You, ", h.user_path(h.current_user))
+    end
+    if comment.likes.count < 3
+      comment.likes.each do |like|
+        if like.user_id != h.current_user.id
+          like_text += h.link_to(" #{users_name(like.user_id)},", h.user_path(like.user_id))
+        end
+      end
+    elsif comment.likes.count >= 3
+      if current_user_like?
+        like_text += " and #{comment.likes.count - 1} other users"
+      else
+        like_text += "#{comment.likes.count} other users"
+      end
+    end
+    like_text += " like this comment"
+    if comment.likes.count == 0
+      like_text = ""
+    end
+    like_text.html_safe
+  end
+
 end
