91a99146998b57a259e2ebc543a820c039aeabdd
Jeffrey Gisin <jgisin@gmail.com>
2016-02-24 15:03:15 -0700

    photo deleting

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -17,6 +17,16 @@ class PhotosController < ApplicationController
     @photo = Photo.find(params[:id])
   end
 
+  def destroy
+    @photo = Photo.find(params[:id])
+    if @photo.destroy
+      flash[:success] = "Photo Deleted"
+    else
+      flash[:error] = "Photo Deleting unsuccessful"
+    end
+    redirect_to new_user_photo_path(@photo.user)
+  end
+
   private
 
   def photo_params
